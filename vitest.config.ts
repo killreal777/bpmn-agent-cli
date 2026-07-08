60dd509677d05bf809e19f4f6f7d2d753961bca0
Kirill Kravtsov <killreal777.personal@gmail.com>
2026-07-08 15:49:25 +0300

    test: increase cli test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
-    pool: 'forks'
+    pool: 'forks',
+    testTimeout: 15000
   }
 });
